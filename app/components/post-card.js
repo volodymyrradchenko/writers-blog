6c47d949c6010b46d5be13cce702fefcb63e5d72
rreckonerr <rreckonerr@gmail.com>
2018-10-21 17:38:18 +0300

    add index page content components

--- app/components/post-card.js
+++ app/components/post-card.js
@@ -1,17 +1,8 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 
 export default Component.extend({
-  tagName: 'li',
-  classNames: ['posts__item', 'infinite-scroll__item'],
-
-  imgUrl: '',
-  imgAlt: '',
-  imgCaption: '',
-  category: '',
-  title: '',
-
-  dateTime: null,
-  dateTimeShow: null,
-  readingTime: null,
+  // TODO add dateTime, dateTimeShow and readingTime calculations
+  dateTime: '',
+  dateTimeShow: '',
+  readingTime: ''
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,36 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  // model() {
-  //   return this.store.findAll('post');
-  // }
+  model() {
+    this.store.push({
+      data: [{
+        id: 1,
+        type: 'post',
+        attributes: {
+          title: 'testing',
+          body: 'blablabla',
+          imgUrl: 'https://s26162.pcdn.co/wp-content/uploads/2015/08/big-bukowski-1240x616.jpg',
+          imgAlt: 'lknjklsajf',
+          imgCapt: 'jhkjhkj',
+          category: 'asfasf',
+          timestamp: '123124255125',
+        }
+      },
+        {
+          id: 2,
+          type: 'post',
+          attributes: {
+            title: 'hoho',
+            body: 'blablabla',
+            imgUrl: 'https://s26162.pcdn.co/wp-content/uploads/2015/08/big-bukowski-1240x616.jpg',
+            imgAlt: 'lknjklsajf',
+            imgCapt: 'jhkjhkj',
+            category: 'asfasf',
+            timestamp: '123124255125',
+        }
+      }]
+    });
+    return this.store.findAll('post');
+
+  }
 });
