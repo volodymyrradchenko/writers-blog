f34f5ea174ce1e8277556f43ddb254e2ae5fa231
rreckonerr <rreckonerr@gmail.com>
2018-10-22 12:20:44 +0300

    pass model to post-card component

--- app/components/post-card.js
+++ app/components/post-card.js
@@ -1,8 +1,11 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   // TODO add dateTime, dateTimeShow and readingTime calculations
   dateTime: '',
-  dateTimeShow: '',
+  dateTimeShow: computed(function() {
+    return this.get('title');
+  }),
   readingTime: ''
 });
