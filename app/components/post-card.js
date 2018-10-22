cbb5eb5d7a22b266299cb64fd2a0203d5390b005
rreckonerr <rreckonerr@gmail.com>
2018-10-22 16:29:11 +0300

    minor html/css refactoring for post-card

--- app/components/post-card.js
+++ app/components/post-card.js
@@ -2,6 +2,8 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
+  tagName: 'article',
+  classNames: ['posts__item', 'infinite-scroll__item'],
   // TODO add dateTime, dateTimeShow and readingTime calculations
   dateTime: '',
   dateTimeShow: computed(function() {
