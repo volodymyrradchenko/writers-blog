5aebe0f183c873aa0cc7b520a952fdf0c151d6ae
rreckonerr <rreckonerr@gmail.com>
2018-10-28 15:41:10 +0200

    add post comment functionality

---
+++ app/routes/application.js
@@ -0,0 +1,12 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { debug } from '@ember/debug';
+
+export default Route.extend({
+  session: service(),
+
+  beforeModel() {
+    return this.get('session').fetch().catch((err) => debug('---oops===>   ' + err));
+  }
+
+});
--- app/routes/posts/post.js
+++ app/routes/posts/post.js
@@ -1,7 +1,31 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { debug } from '@ember/debug';
+
+
+import RSVP from 'rsvp';
 
 export default Route.extend({
-  model(params) {
-    return this.store.findRecord('post', params.post_id);
+  store: service(),
+  session: service(),
+
+  model( { post_id } = {} ) {
+    return RSVP.hash({
+      post: this.store.findRecord('post', post_id),
+      comment: this.store.createRecord('comment')
+    });
+  },
+
+  actions: {
+    saveComment(newComment) {
+      let currentPost = this.get('currentModel').post;
+      newComment.set('uid', this.session.get('currentUser').uid);
+
+      currentPost.get('comments').pushObject(newComment);
+
+      newComment.save().then(function() {
+        currentPost.save()
+      }).then(() => this.refresh());
+    }
   }
 });
