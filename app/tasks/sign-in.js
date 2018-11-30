4d45167953f5a84fc587152b7d432da3afaa9e76
rreckonerr <rreckonerr@gmail.com>
2018-11-30 15:17:25 +0200

    fix new user in database on every signs-in

--- app/tasks/sign-in.js
+++ app/tasks/sign-in.js
@@ -2,12 +2,26 @@ import { inject as service } from '@ember/service';
 import { get } from '@ember/object';
 import { task } from 'ember-concurrency';
 
+import firebase from 'firebase';
+
 export default task({
   store: service(),
   session: service(),
 
   *perform(provider = '', email = '', password = '') {
     try {
+
+      let isNewUser = yield (firebase_response = {}) => {
+        return new Promise(resolve => {
+          firebase.database().ref('users').orderByChild('uid').equalTo(firebase_response.currentUser.uid).on('value', snapshot => {
+            if (snapshot.val() === null) {
+              resolve(firebase_response)
+            }
+          })
+        })
+      }
+
+
       if (provider === 'password') {
         yield get(this, 'session').open('firebase', {
           provider: provider,
@@ -21,12 +35,17 @@ export default task({
         yield get(this, 'session').open('firebase', {
           provider: provider,
         }).then((response) => {
+
+          isNewUser(response).then(response => {
+
             get(this, 'store').createRecord('user', {
               uid: get(response, 'currentUser.uid'),
               email: get(response, 'currentUser.email'),
               name: get(response, 'currentUser.displayName'),
               avatar: get(response, 'currentUser.photoURL')
             }).save()
+
+          }).catch(error => console.log(get(error)))
         });
       }
 
